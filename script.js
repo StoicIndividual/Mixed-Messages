254cfe197434efc6ace940d41f2fddbe42cfa64b
wim <WimVeto@protonmail.com>
2022-02-09 14:41:00 +0100

    add script funcionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const quotes = ["When you have a dream, you’ve got to grab it and never let go. — Carol Burnett", "Nothing is impossible. The word itself says Im possible — Audrey Hepburn", "There is nothing impossible to they who will try. — Alexander the Great", "The bad news is time flies. The good news is you’re the pilot. — Michael Altshuler"];
+
+let randomNum = Math.floor(Math.random()*quotes.length);
+
+console.log(quotes[randomNum]);
+
